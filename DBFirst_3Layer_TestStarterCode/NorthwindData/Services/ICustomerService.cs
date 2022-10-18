c5b9716575a6a80e6268983a0a630abfe2170944
Rob <robertma.green@gmail.com>
2022-10-18 15:47:06 +0100

    adding service layer

---
+++ DBFirst_3Layer_TestStarterCode/NorthwindData/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindData.Services
+{
+    public interface ICustomerService
+    {
+        List<Customer> GetCustomerList();
+        Customer GetCustomerById(string customerId);
+        void CreateCustomer(Customer c);
+        void SaveCustomerChanges();
+        void RemoveCustomer(Customer c);
+    }
+}
