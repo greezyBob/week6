dee80f341faf2c27f2e20c734066e3dcb1353e16
Rob <robertma.green@gmail.com>
2022-10-20 15:12:11 +0100

    todoapi

---
+++ ToDoApi/ToDoApi/Models/TodoItemContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi.Models
+{
+    public class TodoItemContext : DbContext
+    {
+        public TodoItemContext(DbContextOptions<TodoItemContext> options) : base(options)
+        {
+        }
+
+        protected TodoItemContext()
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
